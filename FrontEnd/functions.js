5240cab6ae57a96b45ad904a4aa92a7b6eeddc09
Samir2113 <52280166+Samir2113@users.noreply.github.com>
2024-07-01 12:23:56 +0200

    Intégration de la page de login (non fonctionnelle)

--- FrontEnd/functions.js
+++ FrontEnd/functions.js
@@ -31,3 +31,4 @@ export function filtrerTravaux(categories, worksList, galleryZone){
     }    
 }
 
+
--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,11 +1,28 @@
 import { afficherTravaux, filtrerTravaux } from "./functions.js";
 
-const reponse = await fetch("http://localhost:5678/api/works");
-const works = await reponse.json();
+const reponseWorks = await fetch("http://localhost:5678/api/works");
+const works = await reponseWorks.json();
+const logins = {
+    "email": "sophie.bluel@test.tld",
+    "password": "S0phie"
+};
+const requestLogin = JSON.stringify(logins);
+const reponseLogin = await fetch("http://localhost:5678/api/users/login",{
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: requestLogin
+});
+const test = await reponseLogin.json();
+console.log(reponseLogin.status);
+console.log(test);
+window.sessionStorage.setItem('tokenName',test.token);
+
+// test.then(({token, userId}) => console.log({token, userId}));
+
 const worksZone = document.querySelector(".gallery");
 const filterList = document.querySelectorAll("#filter li");
 // console.log(filterList);
-// console.log(works);
+
 
 
 afficherTravaux(works, worksZone);
